da345272e9e4533af93578f7875719609c85e46b
again7536 <45936642+again7536@users.noreply.github.com>
2020-02-21 18:26:05 +0900

    Change Name

--- ImageClassify.py
+++ ImageClassify.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+####################################### Preprocessing #######################################
 #shape =[32, 32, 3]
 def unpickle(file):
     import pickle 
@@ -16,7 +18,7 @@ def unpickle(file):
 
 #training variables 
 epochs = 40
-batch_size = 200
+batch_size = 512
 nb_class = 10
 
 X_data = [] ; Y_data = []
@@ -26,9 +28,18 @@ for i in range(1, 6):
 X_data = np.array(X_data) ; Y_data = np.array(Y_data)
 X_test, Y_test = unpickle("CIFAR-10/test_batch")
 
+#input standardization
+mean = X_data.mean()
+std = X_data.std()
+X_data = (X_data - mean) / std
+X_test = (X_test - mean) / std
+
+#placeholder
 X = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3])
 Y = tf.placeholder(dtype=tf.float32, shape=[None, nb_class])
 keep_prob = tf.placeholder(tf.float32)
+
+########################################## Model ###########################################
 
 with tf.variable_scope('Layer1'):
     W1 = tf.Variable(tf.random_normal(shape=[3, 3, 3, 64], stddev=0.01), name='W1')
@@ -36,7 +47,7 @@ with tf.variable_scope('Layer1'):
     L1 = tf.nn.relu(L1, name='Lelu1')
     L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1,2,2,1], padding='SAME', name='Pool1')
     L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
-    L1 = tf.layers.batch_normalization(L1)
+    #L1 = tf.layers.batch_normalization(L1)
     #shape reduces to [16, 16, 32]
 
 with tf.variable_scope('Layer2'):
@@ -54,27 +65,29 @@ with tf.variable_scope('Layer3'):
     L3 = tf.nn.relu(L3, name='Lelu3')
     L3 = tf.nn.max_pool(L3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name='Pool3')
     L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
-    L3 = tf.layers.batch_normalization(L3)
+    #L3 = tf.layers.batch_normalization(L3)
     #shape reduces to [4, 4, 128]
     L3 = tf.reshape(L3, shape=[-1, 4*4*256])
 
-with tf.variable_scope('Layer6'):
-    W6 = tf.get_variable("W6", shape=[4*4*256, 512], initializer=tf.glorot_normal_initializer())
+with tf.variable_scope('Layer4'):
+    W4 = tf.get_variable("W4", shape=[4*4*256, 512], initializer=tf.glorot_normal_initializer())
     b = tf.Variable(tf.random_normal([512]))
-    L6 = tf.nn.relu(tf.matmul(L3, W6) + b, name='Lelu6')
-    L6 = tf.nn.dropout(L6, keep_prob=keep_prob)
-    L6 = tf.layers.batch_normalization(L6)
+    L4 = tf.nn.relu(tf.matmul(L3, W4) + b, name='Lelu4')
+    L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+    L4 = tf.layers.batch_normalization(L4)
 
-with tf.variable_scope('Layer7'):
-    W7 = tf.get_variable("W7", shape=[512, 10], initializer=tf.glorot_normal_initializer())
+with tf.variable_scope('Layer5'):
+    W5 = tf.get_variable("W5", shape=[512, 10], initializer=tf.glorot_normal_initializer())
     b2 = tf.Variable(tf.random_normal([10]))
-    hypothesis = tf.matmul(L6, W7) + b2
+    hypothesis = tf.matmul(L4, W5) + b2
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(cost)
 
 correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+###################################### Training ##########################################
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -86,7 +99,7 @@ with tf.Session() as sess:
             batch_y = Y_data[epoch % 5][i*batch_size:(i+1)*batch_size]
             _, c, a = sess.run([optimizer, cost, accuracy], feed_dict={X:batch_x, Y:batch_y, keep_prob:0.5})
             avg_cost += c / batch_num
-        print(f'Epoch: {epoch}\t Cost: {avg_cost:.6f}\t Accuracy:{a:.4f}')
+        print(f'Epoch: {epoch}\t Cost: {avg_cost:.6f}\t Accuracy: {a:.4f}')
 
     print('Accuracy:', sess.run(accuracy, feed_dict={X:X_test[0:batch_size], Y:Y_test[0:batch_size], keep_prob:1.0}))
 
